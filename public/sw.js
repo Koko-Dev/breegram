2d47776e65bb64575467a3bb27c6cbc5db495cde
Koko Dev <koko.webdev@gmail.com>
2018-11-14 15:37:55 -0500

    In sw.js, add Comments explaining the functionality of the claim() method -- self.clients.claim() -- in the 'acivate' event

--- public/sw.js
+++ public/sw.js
@@ -6,9 +6,16 @@ self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating Service Worker ...', event);
   
   /*
-  * claim() method of 'Clients' allows active SW to set itself as the 'controller'
-  * for all clients within this 'scope'.
-  * -- Triggers a "controllerchange" event */
+    From: https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim
+  * -- claim() method of 'Clients' allows active SW to set itself as the 'controller'
+  *       for all clients within this 'scope'.
+  * -- Triggers a "controllerchange" event on 'navigator.serviceWorker' in any clients
+  *       that become controlled by this SW.
+  * -- When an SW is initially registered, pages won't use it until they next load.
+  *       The claim() method causes those pages to be controlled immediately.
+  *        -- Be aware that this results in your SW controlling pages that loaded
+  *           regularly over the network, or possibly via a different SW.
+*/
   return self.clients.claim();
 });
 
