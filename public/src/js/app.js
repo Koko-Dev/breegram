e969ada41a32fc447056d63be8b6ba2ba86676d6
Koko Dev <koko.webdev@gmail.com>
2019-02-24 19:49:34 -0500

    In the app.js configPushSubscription() function, access the push manager to check if there is either an existing subscription or no subscription; if there is no subscription, create one and add an object to subscribe() that would protect the URL endpoint so that no one can send a message to the user that does not originate from my backend server
    This function is not complete

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -82,6 +82,85 @@ function configPushSubscription() {
 //  First ask if I do not have access to the Service Worker
     if (!('serviceWorker' in navigator)) return;
 
+    /*
+        Access the Service Worker from here and then access
+          the pushManager to check for an existing subscription
+     */
+
+    // Will store the Service Worker Registration for later use
+    let registration;
+
+    navigator.serviceWorker.ready
+        .then(swregistration => {
+          // Store the Service Worker Registration for later use
+            registration = swregistration;
+          /*
+              Check for any existing subscription
+              getSubscription returns a promise which will resolve
+              with any subscriptions fetched and null otherwise
+
+              The pushManager will only check for subscriptions
+              for this browser on this device
+          */
+          return swregistration.pushManager.getSubscription();
+        })
+        .then(subscriptions => {
+          /*
+
+              A subscription contains the URL endpoint of the
+              browser vendor server to which we push our push messages.
+              Anyone with this endpoint URL can send messages to that
+              server and this server will forward them to the app.
+              A hacking nightmare.
+
+              Protect by passing a configuration
+              to subscribe() with a JavaScript Object.
+
+
+              Null if there are no subscriptions on this browser
+              for this device (the service worker is also
+              registered in this browser for this device)
+
+              Each browser/device combination yields one
+              subscription
+
+              If subscriptions is null, create a new subscription.
+              If subscriptions is not null then we have existing
+              subscriptions.
+          */
+          if (subscriptions === null) {
+            /*
+                Create a new subscription by accessing the pushManager
+                If there were a previous subscription, it would replace it.
+
+                Protection:
+                    --Pass JS Object, setting userVisibility to true.
+                      This ensures that messages can only come from our
+                      backend server and that, should someone attain our
+                      endpoint URL, they would not be able to push messages
+                      to the API endpoint by the browser vendor server.
+
+                      So, the security mechanism is that I will identify my
+                      backend server as the only valid source sending push
+                      notifications to the users
+            */
+
+              registration.pushManager.subscribe({
+                  userVisibleOnly: true
+              });
+
+
+          } else {
+           /* We have a subscription.
+
+              We can send it to the Service Worker here for an update
+              or ignore it because we already have it stored on the
+              backend server
+           */
+
+          }
+        })
+
 }
 
 
