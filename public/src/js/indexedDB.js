b98f63bd853776db1e50e96bf791e1c209f1276a
Koko Dev <koko.webdev@gmail.com>
2019-04-08 14:54:17 -0400

    Add web-push capability, generate VAPID keys, and create helper function in indexedDB.js to convert urlBase64 VAPID public key into ArrayBuffer which is required by the options object in the subscribe() API used in app.js configPushSubscription() function

--- public/src/js/indexedDB.js
+++ public/src/js/indexedDB.js
@@ -84,4 +84,22 @@ function deleteSingleItemFromIdbStore(storeName, id) {
     })
 }
 
+/*  Used to convert the VAPID urlBase64 public key into
+*   an ArrayBuffer as required by the subscribe() API
+*   */
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
 
