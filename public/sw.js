8ece8c466e7108d33139435509f6c823acc47e0a
Koko Dev <koko.webdev@gmail.com>
2019-04-20 15:16:18 -0400

    Update Service Worker after Firebase Deploy

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@ importScripts('/src/js/idb.js');
 importScripts('/src/js/indexedDB.js');
 
 
-const STATIC_CACHE = 'static-v91';
-const DYNAMIC_CACHE = 'dynamic-v87';
+const STATIC_CACHE = 'static-v96';
+const DYNAMIC_CACHE = 'dynamic-v92';
 
 // for storing request.url's in the cache, not file paths
 const STATIC_FILES = [
