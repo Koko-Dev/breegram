724ff8a939620577f4b0e2cbecad4f13d780e08a
Koko Dev <koko.webdev@gmail.com>
2019-04-22 17:25:18 -0400

    In Service Worker sync listener, add rawLocation for latitude and longitude in loop to send to the server (and to eventually be stored in indexedDB store)

--- public/sw.js
+++ public/sw.js
@@ -400,6 +400,8 @@ self.addEventListener('sync', event => {
                     postData.append('id', dt.id);
                     postData.append('title', dt.title);
                     postData.append('location', dt.location);
+                    postData.append('rawLocationLat', dt.rawLocation.lat);
+                    postData.append('rawLocationLng', dt.rawLocation.lng);
 
                     // Override dt.picture with dt.id + '.png'
                     //   Defaults to a png
