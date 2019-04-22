fa2bc386498f6bbd12ccc1e3a2a9125917e118f0
Koko Dev <koko.webdev@gmail.com>
2019-04-22 17:30:08 -0400

    In Service Worker sync event, remove postData.append to location since we are now using rawLocation for latitude and longitude

--- public/sw.js
+++ public/sw.js
@@ -399,7 +399,7 @@ self.addEventListener('sync', event => {
                     let postData =  new FormData();
                     postData.append('id', dt.id);
                     postData.append('title', dt.title);
-                    postData.append('location', dt.location);
+                    // postData.append('location', dt.location);
                     postData.append('rawLocationLat', dt.rawLocation.lat);
                     postData.append('rawLocationLng', dt.rawLocation.lng);
 
